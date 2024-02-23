76bb576648654e3510e9aec9953916d6fd0f1f2c
mars01253 <mauadtoto001@gmail.com>
2024-02-23 12:09:40 +0100

    configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,7 @@
 import './bootstrap';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a69b2818b4705e752469399df88b92fd0f234bb5
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
-<<<<<<< HEAD
->>>>>>> d770067ed94c9c727b2772442ef1c08376d3abed
-=======
->>>>>>> a69b2818b4705e752469399df88b92fd0f234bb5
