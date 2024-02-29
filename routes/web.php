741d265670f4e9175918b9bab2bddee465df67df
mars01253 <mauadtoto001@gmail.com>
2024-02-29 12:20:38 +0100

    last version

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/update-message-auto-delete/{userId}', [ProfileController::class, 'updateMessage'])->name('profile.update-message-auto-delete');
 });
 
 require __DIR__.'/auth.php';
